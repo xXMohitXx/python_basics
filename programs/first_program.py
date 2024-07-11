8e04f16f3d8a5bf5586666fe50465b3a39635490
mohit <molimanglani756@gmail.com>
2024-07-11 18:48:06 +0530

    commit

---
+++ programs/first_program.py
@@ -0,0 +1,4 @@
+print('hello world')
+#this is how to write a single line comment in python
+'''this is how to comment a multi line in python'''
+print('the above line is commented')
---
+++ programs/operators.py
@@ -0,0 +1,24 @@
+#Arithmatic operators
+num1 = 8
+num2 = 3
+print("add", num1 + num2) #11
+print("sub", num1 - num2) #5
+print("mul", num1 * num2) #24
+print("div", num1 / num2) #2.666
+print("mod", num1 % num2) #2
+
+#relational operators
+print(num1 == num2) #false
+print(num1 != num2) #true
+#print(num1 <> num2) <> this is not supported
+print(num1 <= num2) #false
+print( num1 >= num2) #true
+
+#the concate operator
+str1 ='wow ' + 'python'
+print(str1)
+
+#the print function with end argument used to make the print statements in one line
+print('Hello', end=' ')
+print('world', end=' ')
+print('good bye')
